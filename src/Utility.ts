40755171c9452315bea1662a1ed2052dc4061ef7
manish5914 <50402891+manish5914@users.noreply.github.com>
2023-10-08 18:38:51 +0400

    Code Clean up | well i tried

--- src/Utility.ts
+++ src/Utility.ts
@@ -1,8 +1,12 @@
-export enum DefaultTime {
-    startTime = "00:00:00",
-    endTime = "00:00:00"
-}
+export const REG_EX_TIME: RegExp = /([0-9]?[0-9]):([0-9][0-9]):([0-9][0-9])/;
+export const DEFAULT_DATE: string = "1970-01-01T";
+export const DEFAULT_TIME: string = "00:00:00";
+export const DEFAULT_TIME_FORMAT: string = "HH:mm:ss";
+export const DEFAULT_TIMESHEET_CODE: string = "Work";
 
+export enum Hours{
+    ZeroHour = 0
+}
 export const Log = (message: string, data: any, setLogMessage?: Function):void => {
     console.log(message, data);
     if(setLogMessage)
@@ -10,4 +14,25 @@ export const Log = (message: string, data: any, setLogMessage?: Function):void =
 }
 export const ProcessDate = (date: Date): string => {
     return date.toISOString().substring(0, 10);
+}
+
+export const CalculateHours = (startTime: string, endTime: string): number =>{
+
+    if((startTime && startTime === DEFAULT_TIME ) || (endTime && endTime === DEFAULT_TIME)){
+        return Hours.ZeroHour;
+    }
+    
+    if(!IsCorrectTimeFormat(startTime) || !IsCorrectTimeFormat(endTime))
+        return Hours.ZeroHour;
+
+    let x = new Date(DEFAULT_DATE + startTime);
+    let y = new Date(DEFAULT_DATE + endTime);
+
+    return y.getTime() - x.getTime();
+}
+
+const IsCorrectTimeFormat = (time: string): boolean => {
+    if (!time)
+        return false;
+    return REG_EX_TIME.test(time);  
 }
